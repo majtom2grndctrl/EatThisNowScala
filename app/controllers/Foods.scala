3273b318c8d55e96674a86e163378848969335a3
Dan Hiester <dhiester@distantlyyours.com>
2012-01-17 12:59:39 -0500

    Input form compiles without throwing error.

--- app/controllers/Foods.scala
+++ app/controllers/Foods.scala
@@ -14,6 +14,8 @@ import models._
 
 object Foods extends Controller with Secured {
 
+  val Home = Redirect(routes.Foods.index())
+
   val foodForm = Form(
     of(Food.apply _, Food.unapply _)(
       "id" -> ignored(NotAssigned),
@@ -30,11 +32,21 @@ object Foods extends Controller with Secured {
       Ok(
         html.foods.index(
           Food.findFoodFor(username),
-          user,
-          foodForm
+          user
         )
       )
     }.getOrElse(Forbidden)
+  }
+
+  def create = IsAuthenticated { username => _ =>
+  	User.findByEmail(username).map { user =>
+  		Ok(
+  		    html.foods.create(
+  		        foodForm,
+  		        user
+  		    )
+  		)
+  	}.getOrElse(Forbidden)
   }
 
   def markAsEaten(food: Long) = IsOwnerOf(food) { _ => implicit request =>
@@ -43,15 +55,18 @@ object Foods extends Controller with Secured {
     Ok
     Redirect(routes.Foods.index)
   }
-/*
-  def save = Action { implicit request =>
-    foodForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.foods.index(formWithErrors)),
-      food => {
-        Food.insert(food)
-        Home.flashing("success" -> "Food %s has been created".format(food.name))
-      }
-    )
+
+  def save = IsAuthenticated { username => implicit request =>
+    User.findByEmail(username).map { user =>
+	    foodForm.bindFromRequest.fold(
+	      formWithErrors => BadRequest(html.foods.create(formWithErrors, user)),
+	      food => {
+	        Food.create(food)
+	        Home.flashing("success" -> "Food %s has been created".format(food.name))
+	      }
+	    )
+
+    }.getOrElse(Forbidden)
+
   }
-*/
 }
