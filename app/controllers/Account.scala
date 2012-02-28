8b9a916a15de466ba94fd0ccd54d71d24513f6fa
Dan Hiester <dhiester@distantlyyours.com>
2012-02-28 13:31:15 -0500

    Pre-populate account admin form with user's information.

--- app/controllers/Account.scala
+++ app/controllers/Account.scala
@@ -15,20 +15,37 @@ object Account extends Controller with Secured {
 
   val accountForm = Form(
     tuple(
-      "password1" -> text,
-      "password2" -> text
-    )
+      "email" -> email,
+	  "password" -> tuple(
+	    "main" -> text(minLength = 6),
+	    "confirm" -> text
+	  ).verifying(
+	    "Passwords don't match", passwords => passwords._1 == passwords._2
+	  )
+	)
   )
 
   def manage = IsAuthenticated { username => implicit request =>
     User.findByEmail(username).map { user =>
+      val existingUser: (String, (String, String)) = (user.email, (user.password, user.password))
       Ok(
         html.account.index(
-          accountForm,
+          accountForm.fill(existingUser),
           user
         )
       )
     }.getOrElse(Forbidden)
   }
-
+/*
+  def update = IsAuthenticated { username => implicit request =>
+    User.findByEmail(username).map { user =>
+      Form(
+        "email" -> nonEmptyText
+      ).bindFromRequest.fold(
+        errors => BadRequest,
+        Ok(html.account.summary(email, user))
+      )
+    }
+  }
+*/
 }
