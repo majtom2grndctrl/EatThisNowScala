4bd5de2d31002aebdc5c520ac0ce14fabdac00e3
Dan Hiester <dhiester@distantlyyours.com>
2012-02-29 12:58:44 -0500

    Account management screen at least doesn't throw errors. Doesn't work, either.

--- app/controllers/Account.scala
+++ app/controllers/Account.scala
@@ -17,8 +17,8 @@ object Account extends Controller with Secured {
     tuple(
       "email" -> email,
 	  "password" -> tuple(
-	    "main" -> text(minLength = 6),
-	    "confirm" -> text
+	    "password1" -> text(minLength = 6),
+	    "password2" -> text
 	  ).verifying(
 	    "Passwords don't match", passwords => passwords._1 == passwords._2
 	  )
@@ -29,21 +29,20 @@ object Account extends Controller with Secured {
     User.findByEmail(username).map { user =>
       val existingUser: (String, (String, String)) = (user.email, (user.password, user.password))
       Ok(
-        html.account.index(
-          accountForm.fill(existingUser),
+        html.account.manage(
+          accountForm/*.fill(existingUser)*/,
           user
         )
       )
     }.getOrElse(Forbidden)
   }
-/*
+
   def update = IsAuthenticated { username => implicit request =>
     User.findByEmail(username).map { user =>
       accountForm.bindFromRequest.fold(
-        errors => BadRequest,
-        Ok(html.account.summary(email, main, confirm, user))
+        errors => BadRequest(html.account.manage(errors, user)),
+        {case(email, (password1, password2)) => Ok(html.account.summary(email, password1, password2, user))}
       )
-    }
+    }.getOrElse(Forbidden)
   }
-*/
 }
