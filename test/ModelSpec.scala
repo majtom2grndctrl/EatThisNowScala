43736dc4648b4cea65395a5b4c9cf2f999c5b6d4
Dan Hiester <dhiester@distantlyyours.com>
2012-03-28 21:36:49 -0400

    One more model unit test. Some additional housekeeping.

--- test/ModelSpec.scala
+++ test/ModelSpec.scala
@@ -28,10 +28,35 @@ class ModelSpec extends Specification {
         mashedPotatoes.owner must equalTo(Id(1))
       }
     }
+
+    "Add new food to test user's list" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+	
+        Food.create(Food(NotAssigned, "Test Food", false, Id(1), date("11/19/2012")))
+	
+        val Some(testFood) = Food.findById(1005)
+	
+        testFood.name must equalTo("Test Food")
+        testFood.eaten must equalTo(false)
+        testFood.owner must equalTo(Id(1))
+        testFood.expiry must equalTo(date("11/19/2012"))
+      }
+    }
+
+    "Mark food as eaten" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+	
+        Food.markAsEaten(1000, true)
+
+        val Some(testFood) = Food.findById(1000)
+	
+        testFood.eaten must equalTo(true)
+      }
+    }
   }
 
   "User model" should {
-    "be retrieved by id" in {
+    "retrieve user by id" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
 
         val Some(testUser) = User.findById(1: Long)
@@ -41,36 +66,23 @@ class ModelSpec extends Specification {
         testUser.email must equalTo("test@example.com")
         testUser.password must equalTo("secret")
         testUser.id must equalTo(Id(1))
-
       }
     }
-  }
 
-  "Add new food to test user's list" in {
-    running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-	
-      Food.create(Food(NotAssigned, "Test Food", false, Id(1), date("11/19/2012")))
-	
-      val Some(testFood) = Food.findById(1005)
-	
-      testFood.name must equalTo("Test Food")
-      testFood.eaten must equalTo(false)
-      testFood.owner must equalTo(Id(1))
-      testFood.expiry must equalTo(date("11/19/2012"))
+    "retreive all users" in {
+      running(FakeApplication()) {
+
+        val users: Seq[User] = User.findAll
+
+        users must equalTo(
+          List(
+            User(Id(1),"test@example.com","Test","User","secret"),
+            User(Id(2),"dhiester@example.com","Dan","Hiester","secret"),
+            User(Id(3),"wbuccicone@example.com","Whitney","Buccicone","secret")
+          )
+        )
+      }
     }
+
   }
-
-  "Add new food to test user's list and mark as eaten" in {
-    running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-	
-      Food.create(Food(NotAssigned, "Test Food", false, Id(1), date("11/19/2012")))
-      Food.markAsEaten(1005, true)
-
-      val Some(testFood) = Food.findById(1005)
-	
-      testFood.eaten must equalTo(true)
-    }
-  }
-
-
 }
